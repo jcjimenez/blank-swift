1384975d48f03baf6a0fb2b061f7e62e5b5f07b8
JC Jimenez <1117904+jcjimenez@users.noreply.github.com>
2023-04-03 17:00:05 -0500

    Addressing linter warnings

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Blank",
-            targets: ["Blank"]),
+            targets: ["Blank"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,6 +23,6 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "BlankTests",
-            dependencies: ["Blank"]),
+            dependencies: ["Blank"])
     ]
 )
