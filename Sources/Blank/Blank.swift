58ea9ee790882d7c8b2d05e61f73fd34d8d91d46
JC Jimenez <1117904+jcjimenez@users.noreply.github.com>
2023-04-11 10:24:12 -0500

    feat(Blank): Introducing ability to add

--- Sources/Blank/Blank.swift
+++ Sources/Blank/Blank.swift
@@ -2,6 +2,10 @@ public struct Blank: Codable {
 
     public private(set) var text = "Hello, World!"
 
+    public func add<T: Numeric>(_ lhs: T, _ rhs: T) -> T {
+        return lhs + rhs + 1
+    }
+
     public init() {
     }
 }
